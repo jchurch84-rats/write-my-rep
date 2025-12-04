be4b928e9cd34250ec2a7588ff69a658390bb66d
jchurch84-rats <jakeramsey84@gmail.com>
2025-12-04 10:35:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask # tiny python webserver that turns your code into a web page - takes requests, hands back HTML
 
-app = Flask(__name__)
-@app.route('/')
+app = Flask(__name__) # This is the rat King: __name__ tells Flask hey, I'm this script, RUN ME AS A SERVER, not some Library
+@app.route('/') # This is a "Decorator" binding the the HTTP GET (or POST) request to '/' to the function below-it's Flask's way of saying handle traffic 
 def home()
 
-  'Hello World'
+  return open('index.html').read()
   
